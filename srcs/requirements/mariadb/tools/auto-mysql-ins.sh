3bc619199a433a29334a8eada3978c71e0d919c0
emmaOa <93877957+emmaOa@users.noreply.github.com>
2023-03-02 21:19:35 +0000

    install and run database

---
+++ srcs/requirements/mariadb/tools/auto-mysql-ins.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+MYSQL_PASS=root
+
+set timeout 10
+spawn mysql_secure_installation
+expect "Enter current password for root (enter for none):"
+send -- "\r"
+expect "Set root password?"
+send -- "y\r"
+expect "New password:"
+send -- "${MYSQL_PASS}\r"
+expect "Re-enter new password:"
+send -- "${MYSQL_PASS}\r"
+expect "Remove anonymous users?"
+send -- "y\r"
+expect "Disallow root login remotely?"
+send -- "y\r"
+expect "Remove test database and access to it?"
+send -- "y\r"
+expect "Reload privilege tables now?"
+send -- "y\r"
