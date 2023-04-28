2b4d8288bb3ec9ec175e08242832eb8394bb635d
emmaOa <imaneouazzani.pro@gmail.com>
2023-04-28 13:48:47 +0100

    wordprees

--- srcs/requirements/mariadb/tools/auto-mysql-ins.sh
+++ srcs/requirements/mariadb/tools/auto-mysql-ins.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-set timeout 10
+
+DB_NAME="mydatabase"
+DB_USER="myuser"
+DB_PASS="mypassword"
+
 service mariadb start
-mysql_secure_installation <<EOF
-
-y
-root
-root
-y
-y
-y
-y
-EOF
-
 mariadb
+
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mysql -u root -e  "CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
+
+bash
+# CREATE DATABASE mydatabase;
+# mysql -u root -p -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'myuser'@'localhost';"
+# mariadbd
+
+
+# docker run -it -v mariadb-volume:/var/lib/mysql im
 
