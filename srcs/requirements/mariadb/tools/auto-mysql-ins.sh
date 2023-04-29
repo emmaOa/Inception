eb9369676bca918e6cd6ee35f369d6891aa44947
emmaOa <imaneouazzani.pro@gmail.com>
2023-04-29 19:46:58 +0100

    database run

--- srcs/requirements/mariadb/tools/auto-mysql-ins.sh
+++ srcs/requirements/mariadb/tools/auto-mysql-ins.sh
@@ -12,7 +12,7 @@ mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mysql -u root -e  "CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
 
-bash
+tail -f /dev/null
 # CREATE DATABASE mydatabase;
 # mysql -u root -p -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'myuser'@'localhost';"
 # mariadbd
