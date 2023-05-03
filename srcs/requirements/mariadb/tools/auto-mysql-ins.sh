1a0cee912a7c84a5b1eaa6d9c59be1a34a666ac5
emmaOa <imaneouazzani.pro@gmail.com>
2023-05-03 03:34:30 +0100

    use mysqld and wordpress modification

--- srcs/requirements/mariadb/tools/auto-mysql-ins.sh
+++ srcs/requirements/mariadb/tools/auto-mysql-ins.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 
-DB_NAME="mydatabase"
-DB_USER="myuser"
-DB_PASS="mypassword"
+# DB_NAME="mydatabase"
+# DB_USER="myuser"
+# DB_PASS="mypassword"
 
 service mariadb start
-mariadb
 
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mysql -u root -e  "CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
+mariadb
+service mariadb stop
 
-tail -f /dev/null
+mysqld
 # CREATE DATABASE mydatabase;
 # mysql -u root -p -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'myuser'@'localhost';"
 # mariadbd
