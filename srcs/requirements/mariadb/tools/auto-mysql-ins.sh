198779c46dc7618fe940e632e066c015fdff45ea
emmaOa <imaneouazzani.pro@gmail.com>
2023-05-12 04:03:17 +0100

    nginx

--- srcs/requirements/mariadb/tools/auto-mysql-ins.sh
+++ srcs/requirements/mariadb/tools/auto-mysql-ins.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 
+export DB_NAME=$DB_NAME
+export DB_USER=$DB_USER
+export DB_PASS=$DB_PASS
 # DB_NAME="mydatabase"
 # DB_USER="myuser"
 # DB_PASS="mypassword"
@@ -10,10 +13,13 @@ service mariadb start
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mysql -u root -e  "CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
-mariadb
-service mariadb stop
+# mariadb
+# echo "hiiii"
+# SHOW DATABASES;
+# tail -f /dev/null
+# service mariadb stop
 
-mysqld
+# mysqld
 # CREATE DATABASE mydatabase;
 # mysql -u root -p -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'myuser'@'localhost';"
 # mariadbd
--- srcs/requirements/wordpress/tools/wp-cnf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo hi 
-sed -i "s/define('DB_NAME'.*/define('DB_NAME', 'new_database_name');/g" /var/www/html/wp-config.php
-sed -i "s/define('DB_USER'.*/define('DB_USER', 'new_database_user');/g" /var/www/html/wp-config.php
-sed -i "s/define('DB_PASSWORD'.*/define('DB_PASSWORD', 'new_database_password');/g" /var/www/html/wp-config.php
