11e5bb17d9decfead753c6096b3b89b5fb1d86d9
emmaOa <imaneouazzani.pro@gmail.com>
2023-05-17 05:42:49 +0100

    403

--- srcs/requirements/mariadb/tools/auto-mysql-ins.sh
+++ srcs/requirements/mariadb/tools/auto-mysql-ins.sh
@@ -13,17 +13,5 @@ service mariadb start
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mysql -u root -e  "CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
-# mariadb
-# echo "hiiii"
-# SHOW DATABASES;
-# tail -f /dev/null
-# service mariadb stop
 
-# mysqld
-# CREATE DATABASE mydatabase;
-# mysql -u root -p -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'myuser'@'localhost';"
-# mariadbd
-
-
-# docker run -it -v mariadb-volume:/var/lib/mysql im
 
--- srcs/requirements/nginx/tools/config-nginx.sh
+++ srcs/requirements/nginx/tools/config-nginx.sh
@@ -1,35 +1,26 @@
-!/bin/bash
+#!/bin/bash
 
-touch  /etc/nginx/conf.d/default.conf
-
-echo "http{
-            server {
+echo "server {
         listen 80;
-        server_name iouazzan.42.fr;
-    
+        server_name localhost;
+
         root /var/www/html;
-        index index.php;
-    
+        index index.html index.php;
+
         access_log /var/log/nginx/hakase-access.log;
         error_log /var/log/nginx/hakase-error.log;
-    
+
         location / {
-            try_files $uri $uri/ /index.php?$args;
+            try_files \$uri \$uri/ /index.php?\$query_string;
         }
-    
         location ~ \.php$ {
-            try_files $uri =404;
+            try_files \$uri =404;
             fastcgi_split_path_info ^(.+\.php)(/.+)$;
             fastcgi_pass wordpress:9000;
             fastcgi_index index.php;
             include fastcgi_params;
-            fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-            fastcgi_param PATH_INFO $fastcgi_path_info;
+            fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+            fastcgi_param PATH_INFO \$fastcgi_path_info;
         }
     }
-}
-" > /etc/nginx/conf.d/default.conf
-echo "events {}" >> /etc/nginx/conf.d/default.conf
-
-# echo "hiii" > /etc/nginx/conf.d/default.conf
-# echo "imane" >> /etc/nginx/conf.d/default.conf
+" > /etc/nginx/sites-available/default
